1a2d812a1de1b03ba73687e9c5b5954d5db24dd0
l3w4idiee3 <lewisjassy43@gmail.com>
2023-06-18 13:59:26 +0300

    printing slot machine

--- main.py
+++ main.py
@@ -1,6 +1,38 @@
+import random
 MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
+
+ROWS = 3
+COLS = 3 
+
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8,
+}
+
+def get_slot_machine_spin(row, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+    
+    columns =[[], [], []]
+    for col in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+        column.append(column)
+    return columns
+def print_slot_machine(columns):
+   for row in range(len(column[0])):
+       for i,column in column:
+           if i != len(columns) -1:
+               print(column[row], "|")
 def deposit():
     while True:
         amount = input("What amount would you like to deposit? $")
